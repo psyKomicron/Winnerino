c5c4dce1fe6b8c5ba04932e45df1f6fda00b0557
Julien Marquet <psykomicron@protonmail.com>
2022-04-01 00:20:43 +0200

    cleaned .idl, added dash style home

--- Winnerino/Winnerino/MainPage.xaml.cpp
+++ Winnerino/Winnerino/MainPage.xaml.cpp
@@ -3,6 +3,7 @@
 #if __has_include("MainPage.g.cpp")
 #include "MainPage.g.cpp"
 #endif
+using namespace winrt::Windows::ApplicationModel;
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
@@ -15,20 +16,14 @@ namespace winrt::Winnerino::implementation
     MainPage::MainPage()
     {
         InitializeComponent();
+        PackageVersion version = Package::Current().Id().Version();
+        versionTextBlock().Text(to_hstring(version.Major) + L"." + to_hstring(version.Minor) + L"." + to_hstring(version.Build) + L"." + to_hstring(version.Revision));
+#if defined _DEBUG
+        buildTypeTextBlock().Text(L"Debug");
+#else
+        buildTypeTextBlock().Text(L"Release");
+#endif
     }
 
-    int32_t MainPage::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainPage::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainPage::myButton_Click(IInspectable const&, RoutedEventArgs const&)
-    {
-        myButton().Content(box_value(L"Clicked"));
-    }
+    
 }
