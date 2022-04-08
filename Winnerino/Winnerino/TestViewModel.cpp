acd251e9e3a1f822eab79595ee95de07922bb9db
psykomicron <psykomicron@protonmail.com>
2022-04-08 04:45:53 +0200

    proj update

---
+++ Winnerino/Winnerino/TestViewModel.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "TestViewModel.h"
+#if __has_include("TestViewModel.g.cpp")
+#include "TestViewModel.g.cpp"
+#endif
+
+namespace winrt::Winnerino::implementation
+{
+    int32_t TestViewModel::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void TestViewModel::MyProperty(int32_t /*value*/)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ Winnerino/Winnerino/TestViewModel.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "TestViewModel.g.h"
+
+namespace winrt::Winnerino::implementation
+{
+    struct TestViewModel : TestViewModelT<TestViewModel>
+    {
+        TestViewModel() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::Winnerino::factory_implementation
+{
+    struct TestViewModel : TestViewModelT<TestViewModel, implementation::TestViewModel>
+    {
+    };
+}
