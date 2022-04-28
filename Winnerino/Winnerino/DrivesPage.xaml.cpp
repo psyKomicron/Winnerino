4549cee5313bc65fc92ae6e3ec6f8da22e4e5111
psykomicron <psykomicron@protonmail.com>
2022-04-28 18:36:12 +0200

    added system informations

--- Winnerino/Winnerino/DrivesPage.xaml.cpp
+++ Winnerino/Winnerino/DrivesPage.xaml.cpp
@@ -3,6 +3,7 @@
 #if __has_include("DrivesPage.g.cpp")
 #include "DrivesPage.g.cpp"
 #include "fileapi.h"
+#include "sysinfoapi.h"
 #endif
 using namespace winrt::Microsoft::UI::Xaml::Controls;
 
@@ -24,46 +25,38 @@ namespace winrt::Winnerino::implementation
 
     void DrivesPage::Page_Loaded(IInspectable const&, RoutedEventArgs const&)
     {
+        SYSTEM_INFO systemInfo = SYSTEM_INFO();
+        GetSystemInfo(&systemInfo);
+        switch (systemInfo.wProcessorArchitecture)
+        {
+            case PROCESSOR_ARCHITECTURE_AMD64:
+                CpuXTextBlock().Text(L"AMD/Intel 64");
+                break;
+            case PROCESSOR_ARCHITECTURE_ARM:
+                CpuXTextBlock().Text(L"ARM");
+                break;
+            case PROCESSOR_ARCHITECTURE_ARM64:
+                CpuXTextBlock().Text(L"ARM 64");
+                break;
+            case PROCESSOR_ARCHITECTURE_IA64:
+                CpuXTextBlock().Text(L"Intel Itanium-based 64");
+                break;
+            case PROCESSOR_ARCHITECTURE_INTEL:
+                CpuXTextBlock().Text(L"Intel");
+                break;
+            case PROCESSOR_ARCHITECTURE_UNKNOWN:
+                CpuXTextBlock().Text(L"Unknown");
+                break;
+        }
+        CpuCountTextBlock().Text(to_hstring((uint32_t)systemInfo.dwNumberOfProcessors));
+        CpuLevelTextBlock().Text(to_hstring((uint16_t)systemInfo.wProcessorLevel));
+        CpuRevTextBlock().Text(to_hstring((uint16_t)systemInfo.wProcessorRevision));
+
         ListDrives();
     }
 
     void DrivesPage::ListDrives()
     {
-#if FALSE
-        WCHAR driveName[MAX_PATH]{ 0 };
-        HANDLE findHandle = FindFirstVolume(driveName, MAX_PATH);
-        do
-        {
-            hstring driveGUID = to_hstring(driveName);
-            vector<hstring> driveVolumes = vector<hstring>();
-
-            WCHAR volumes[MAX_PATH]{ 0 };
-            LPWSTR pointer = volumes;
-            DWORD outputLength = 0;
-            if (GetVolumePathNamesForVolumeName(driveName, volumes, MAX_PATH, &outputLength))
-            {
-                uint16_t maxIterations = 0;
-                while (maxIterations < outputLength)
-                {
-                    if (*pointer == NULL)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        driveVolumes.push_back(to_hstring(pointer));
-                    }
-                    maxIterations++;
-                    while (*pointer++);
-                }
-            }
-
-            if (GetVolumeInformation(driveVolumes[0].c_str(), ))
-        } while (FindNextVolume(findHandle, driveName, MAX_PATH));
-        FindVolumeClose(findHandle);
-#endif // 0
-
-
         WCHAR drives[512]{};
         WCHAR* pointer = drives;
         GetLogicalDriveStrings(512, drives); // ignoring return value
