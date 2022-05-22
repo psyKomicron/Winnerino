2c8ca69bf06c9a99c3d6b6e88192b1e754d302f2
psykomicron <psykomicron@protonmail.com>
2022-05-22 15:18:54 +0200

    implementing specialized tabview

--- Winnerino/Winnerino/LibraryTabView.xaml.cpp
+++ Winnerino/Winnerino/LibraryTabView.xaml.cpp
@@ -3,9 +3,13 @@
 #if __has_include("LibraryTabView.g.cpp")
 #include "LibraryTabView.g.cpp"
 #endif
+using namespace winrt::Microsoft::UI::Xaml::Controls;
+using namespace winrt::Windows::Foundation::Collections;
 
 using namespace winrt;
-using namespace Microsoft::UI::Xaml;
+using namespace winrt::Microsoft::UI::Xaml;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::Storage;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -17,18 +21,45 @@ namespace winrt::Winnerino::implementation
         InitializeComponent();
     }
 
-    int32_t LibraryTabView::MyProperty()
+    LibraryTabView::LibraryTabView(hstring const& tag) : LibraryTabView()
     {
-        throw hresult_not_implemented();
+        LoadFiles(tag);
     }
 
-    void LibraryTabView::MyProperty(int32_t /* value */)
+    IAsyncAction LibraryTabView::LoadFiles(hstring const& tag)
     {
-        throw hresult_not_implemented();
-    }
+        StorageFolder documents = nullptr;
 
-    void LibraryTabView::myButton_Click(IInspectable const&, RoutedEventArgs const&)
-    {
-        myButton().Content(box_value(L"Clicked"));
+        if (tag == L"Documents")
+        {
+            documents = KnownFolders::DocumentsLibrary();
+        }
+        else if (tag == L"Music")
+        {
+            documents = KnownFolders::MusicLibrary();
+        }
+        else if (tag == L"Pictures")
+        {
+            documents = KnownFolders::PicturesLibrary();
+        }
+        else if (tag == L"Videos")
+        {
+            documents = KnownFolders::VideosLibrary();
+        }
+        else
+        {
+            co_return;
+        }
+
+        IVectorView<StorageFile> files = co_await documents.GetFilesAsync();
+
+        for (const StorageFile& file : files)
+        {
+            DispatcherQueue().TryEnqueue([this, path = file.Path()]()
+            {
+                FileLargeView view{ path };
+                FilesList().Children().Append(view);
+            });
+        }
     }
 }
