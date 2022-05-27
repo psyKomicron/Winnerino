f42d531e3536604b4d25739ec5ccc47d235bc847
psykomicron <psykomicron@protonmail.com>
2022-05-27 11:39:52 +0200

    updates

--- Winnerino/Winnerino/MainPage.xaml.h
+++ Winnerino/Winnerino/MainPage.xaml.h
@@ -8,10 +8,7 @@ namespace winrt::Winnerino::implementation
     {
         MainPage();
 
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
-
-        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void Button_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
     };
 }
 
