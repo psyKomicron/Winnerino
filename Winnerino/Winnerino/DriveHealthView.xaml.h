9c611760d92d20e892289c11708249b3e96bb07d
psykomicron <psykomicron@protonmail.com>
2022-07-10 02:07:11 +0200

    bug fixes (reloading infos), added more info

--- Winnerino/Winnerino/DriveHealthView.xaml.h
+++ Winnerino/Winnerino/DriveHealthView.xaml.h
@@ -34,36 +34,82 @@ namespace winrt::Winnerino::implementation
             return _driveTotalSpace;
         };
 
+        void ClearRecycleBin_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        winrt::Windows::Foundation::IAsyncAction OpenBinExplorer_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        winrt::Windows::Foundation::IAsyncAction UserControl_Loaded(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+
     private:
-        double _driveFreeSpace;
-        double _driveTotalSpace;
+        bool mainDrive = false;
+        bool firstLoad = true;
+        double _driveFreeSpace = 0;
+        double _driveTotalSpace = 0;
         winrt::hstring _driveName;
-        double _percentage;
+        winrt::hstring drivePath;
+        double _percentage = 0.;
 
         winrt::event<Microsoft::UI::Xaml::Data::PropertyChangedEventHandler> e_propertyChanged;
 
         void DriveName(hstring const& value)
         {
             _driveName = value;
-            e_propertyChanged(*this, PropertyChangedEventArgs{ L"DriveName" });
+            if (DispatcherQueue().HasThreadAccess())
+            {
+                e_propertyChanged(*this, PropertyChangedEventArgs{ L"DriveName" });
+            }
+            else
+            {
+                DispatcherQueue().TryEnqueue([this]()
+                {
+                    e_propertyChanged(*this, PropertyChangedEventArgs{ L"DriveName" });
+                });
+            }
         };
         void DriveFreeSpace(double const& value)
         {
             _driveFreeSpace = value;
-            e_propertyChanged(*this, PropertyChangedEventArgs{ L"DriveFreeSpace" });
+            if (DispatcherQueue().HasThreadAccess())
+            {
+                e_propertyChanged(*this, PropertyChangedEventArgs{ L"DriveFreeSpace" });
+            }
+            else
+            {
+                DispatcherQueue().TryEnqueue([this]()
+                {
+                    e_propertyChanged(*this, PropertyChangedEventArgs{ L"DriveFreeSpace" });
+                });
+            }
         };
         void DriveTotalSpace(double const& value)
         {
             _driveTotalSpace = value;
-            e_propertyChanged(*this, PropertyChangedEventArgs{ L"DriveTotalSpace" });
+            if (DispatcherQueue().HasThreadAccess())
+            {
+                e_propertyChanged(*this, PropertyChangedEventArgs{ L"DriveTotalSpace" });
+            }
+            else
+            {
+                DispatcherQueue().TryEnqueue([this]()
+                {
+                    e_propertyChanged(*this, PropertyChangedEventArgs{ L"DriveTotalSpace" });
+                });
+            }
         };
         void Percentage(double value)
         {
             _percentage = value;
-            e_propertyChanged(*this, PropertyChangedEventArgs{ L"Percentage" });
+            if (DispatcherQueue().HasThreadAccess())
+            {
+                e_propertyChanged(*this, PropertyChangedEventArgs{ L"Percentage" });
+            }
+            else
+            {
+                DispatcherQueue().TryEnqueue([this]()
+                {
+                    e_propertyChanged(*this, PropertyChangedEventArgs{ L"Percentage" });
+                });
+            }
         };
-        void LoadInfos(hstring const& path);
-        void GetSystemFiles(hstring const& path);
+        void LoadInfos();
     };
 }
 
