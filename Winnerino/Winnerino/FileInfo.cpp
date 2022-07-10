109f70017cf4c7f1f4073e15731ef39edc0bc0de
psykomicron <psykomicron@protonmail.com>
2022-07-10 02:00:17 +0200

    created FileInfo

---
+++ Winnerino/Winnerino/FileInfo.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "FileInfo.h"
+
+#include <fileapi.h>
+#include <Shlwapi.h>
+#include "Helpers.h"
+
+namespace Winnerino::Storage
+{
+    FileInfo::FileInfo(WIN32_FIND_DATA* findData, winrt::hstring path)
+    {
+        fileName = winrt::to_hstring(findData->cFileName);
+        parentPath = winrt::hstring{ path };
+        WCHAR filePathWstr[ALTERNATE_MAX_PATH];
+        if (PathCombine(filePathWstr, parentPath.c_str(), fileName.c_str()))
+        {
+            filePath = winrt::to_hstring(filePathWstr);
+        }
+        size = Winnerino::convert_large_uint(findData->nFileSizeHigh, findData->nFileSizeLow);
+        attributes = findData->dwFileAttributes;
+    }
+}
