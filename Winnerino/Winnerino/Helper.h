99bdaf8e7368cc5d08c760bcae5113b8f8b54d73
psykomicron <psykomicron@protonmail.com>
2022-10-04 01:16:26 +0200

    Helper updated format_size
    - Using I18N (ResourceLoader) to return file size extension (GB for en, Go for fr)
    - Corrected b (bit) to B (bytes), since the ui display sizes in bytes

--- Winnerino/Winnerino/Helper.h
+++ Winnerino/Winnerino/Helper.h
@@ -24,45 +24,10 @@ namespace Winnerino
         }
     };
 
-    inline winrt::hstring format_size(double* size)
-    {
-        if (*size >= 0x10000000000)
-        {
-            *size *= (double)100;
-            *size = round(*size / 0x10000000000);
-            *size /= (double)100;
-            return L" Tb";
-        }
-
-        if (*size >= 0x40000000)
-        {
-            *size *= (double)100;
-            *size = round(*size / 0x40000000);
-            *size /= (double)100;
-            return L" Gb";
-        }
-
-        if (*size >= 0x100000)
-        {
-            *size *= (double)100;
-            *size = round(*size / 0x100000);
-            *size /= (double)100;
-            return L" Mb";
-        }
-
-        if (*size >= 0x400)
-        {
-            *size *= (double)100;
-            *size = round(*size / 0x400);
-            *size /= (double)100;
-            return L" Kb";
-        }
-
-        return L" b";
-    };
-
     inline winrt::hstring format_size(double* size, uint16_t const& decimals)
     {
+        static const winrt::Microsoft::Windows::ApplicationModel::Resources::ResourceLoader resLoader{};
+
         double multiplicator = pow(10.0, decimals);
 
         if (*size >= 0x10000000000)
@@ -70,7 +35,7 @@ namespace Winnerino
             *size *= multiplicator;
             *size = round(*size / 0x10000000000);
             *size /= multiplicator;
-            return L" Tb";
+            return resLoader.GetString(L"TerabytesFileSizeExtension");
         }
 
         if (*size >= 0x40000000)
@@ -78,7 +43,7 @@ namespace Winnerino
             *size *= multiplicator;
             *size = round(*size / 0x40000000);
             *size /= multiplicator;
-            return L" Gb";
+            return resLoader.GetString(L"GigabytesFileSizeExtension");
         }
 
         if (*size >= 0x100000)
@@ -86,57 +51,28 @@ namespace Winnerino
             *size *= multiplicator;
             *size = round(*size / 0x100000);
             *size /= multiplicator;
-            return L" Mb";
+            return resLoader.GetString(L"MegabytesFileSizeExtension");
         }
-        
+
         if (*size >= 0x400)
         {
             *size *= multiplicator;
             *size = round(*size / 0x400);
             *size /= multiplicator;
-            return L" Kb";
+            return resLoader.GetString(L"KilobytesFileSizeExtension");
         }
 
-        return L" b";
-    }
+        return resLoader.GetString(L"BytesFileSizeExtension");
+    };
+
+    inline winrt::hstring format_size(double* size)
+    {
+        return format_size(size, 3);
+    };
 
     inline winrt::hstring format_size(double size, uint16_t const& decimals)
     {
-        double multiplicator = pow(10, decimals);
-
-        if (size >= 0x10000000000)
-        {
-            size *= multiplicator;
-            size = round(size / 0x10000000000);
-            size /= multiplicator;
-            return winrt::to_hstring(size) + L" Tb";
-        }
-
-        if (size >= 0x40000000)
-        {
-            size *= multiplicator;
-            size = round(size / 0x40000000);
-            size /= multiplicator;
-            return winrt::to_hstring(size) + L" Gb";
-        }
-
-        if (size >= 0x100000)
-        {
-            size *= multiplicator;
-            size = round(size / 0x100000);
-            size /= multiplicator;
-            return winrt::to_hstring(size) + L" Mb";
-        }
-
-        if (size >= 0x400)
-        {
-            size *= multiplicator;
-            size = round(size / 0x400);
-            size /= multiplicator;
-            return winrt::to_hstring(size) + L" Kb";
-        }
-
-        return winrt::to_hstring(size) + L" b";
+        return format_size(&size, decimals);
     };
 
     inline int64_t convert_large_uint(ULARGE_INTEGER const& largeUInt)
