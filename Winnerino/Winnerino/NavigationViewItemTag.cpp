996bd24d931bab83ec8439dc9efe26f104b7e2cc
psykomicron <psykomicron@protonmail.com>
2022-10-04 01:17:58 +0200

    Created NavigationViewItemTag

---
+++ Winnerino/Winnerino/NavigationViewItemTag.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "NavigationViewItemTag.h"
+#if __has_include("NavigationViewItemTag.g.cpp")
+#include "NavigationViewItemTag.g.cpp"
+#endif
+
+namespace winrt::Winnerino::implementation
+{
+    winrt::hstring NavigationViewItemTag::Action() const
+    {
+        return _action;
+    }
+
+    void NavigationViewItemTag::Action(const winrt::hstring& value)
+    {
+        _action = value;
+    }
+
+    winrt::Windows::Foundation::IInspectable NavigationViewItemTag::Data() const
+    {
+        return _data;
+    }
+
+    void NavigationViewItemTag::Data(const winrt::Windows::Foundation::IInspectable& value)
+    {
+        _data = value;
+    }
+}
---
+++ Winnerino/Winnerino/NavigationViewItemTag.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "NavigationViewItemTag.g.h"
+
+namespace winrt::Winnerino::implementation
+{
+    struct NavigationViewItemTag : NavigationViewItemTagT<NavigationViewItemTag>
+    {
+    public:
+        NavigationViewItemTag() = default;
+
+        winrt::hstring Action() const;
+        void Action(const winrt::hstring& value);
+        winrt::Windows::Foundation::IInspectable Data() const;
+        void Data(const winrt::Windows::Foundation::IInspectable& value);
+
+    private:
+        winrt::hstring _action;
+        winrt::Windows::Foundation::IInspectable _data;
+    };
+}
+
+namespace winrt::Winnerino::factory_implementation
+{
+    struct NavigationViewItemTag : NavigationViewItemTagT<NavigationViewItemTag, implementation::NavigationViewItemTag>
+    {
+    };
+}
