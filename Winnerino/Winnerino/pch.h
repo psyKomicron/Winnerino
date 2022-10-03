3b59a897227d1fd062a0ef774e04c47ddad82744
psykomicron <psykomicron@protonmail.com>
2022-10-04 01:18:20 +0200

    added winrt/Microsoft.UI.Xaml.Documents.h

--- Winnerino/Winnerino/pch.h
+++ Winnerino/Winnerino/pch.h
@@ -24,6 +24,7 @@
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Microsoft.UI.Xaml.Data.h>
+#include <winrt/Microsoft.UI.Xaml.Documents.h>
 #include <winrt/Microsoft.UI.Xaml.Input.h>
 #include <winrt/Microsoft.UI.Xaml.Interop.h>
 #include <winrt/Microsoft.UI.Xaml.Markup.h>
